f10351427d5158a95904ef554e488b59ff0833ef
Stewart ilondanga <stilondanga@gmail.com>
2021-04-15 19:02:28 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,18 @@ var navigate = (function() {
 $('.mv-save').on('click', function() {
   $('.saved').fadeIn().delay(3000).fadeOut();
 });
-/*
+
 function mobileViewUpdate() {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 600) {
-      $('td:not([property="breed"])').each(function(event) {
-        header = $(this).attr('property')
+  var viewportWidth = $(window).width();
+  if (viewportWidth < 600) {
+    $('td:not([property="breed"])').each(function(event) {
+      header = $(this).attr('property')
 
-        if(! $(this).parent().find('#' + header).length) {
-          $('#' + header).clone().insertBefore(this);
-        }
-      });
-    }
+      if (!$(this).parent().find('#' + header).length) {
+        $('#' + header).clone().insertBefore(this);
+      }
+    });
+  }
 }
-
+/*
 $(window).on('load resize', mobileViewUpdate);
