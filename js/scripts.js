e7a0167e0e3ed4d1dabaa6e654af41bda5de0674
Stewart ilondanga <stilondanga@gmail.com>
2021-04-15 19:01:22 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,22 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$('.mv-save').on('click', function() {
+  $('.saved').fadeIn().delay(3000).fadeOut();
+});
+
+function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 600) {
+      $('td:not([property="breed"])').each(function(event) {
+        header = $(this).attr('property')
+
+        if(! $(this).parent().find('#' + header).length) {
+          $('#' + header).clone().insertBefore(this);
+        }
+      });
+    }
+}
+
+$(window).on('load resize', mobileViewUpdate);
